c5b471316838280661344e08efd2b4e565ac062e
pilar <pilarcayo33@gmail.com>
2021-06-05 18:51:45 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-//import logo from './logo.svg';
-//import './App.css';
-//import '../src/Components/NavBar'
 import React from 'react';
 import NavBar from '../src/Components/NavBar';
-
+import ItemListContainer from '../src/Components/ItemListContainer';
 
 function App() {
   return <>       
-        <NavBar />      
+        <NavBar />   
+        <ItemListContainer />   
         </> 
 }
+
 
 export default App;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Container,Row,Col,Form,NavLink } from 'react-bootstrap';
+import { Container,Row,Col,NavLink } from 'react-bootstrap';
 import { FaQuinscape, FaUserAlt } from "react-icons/fa";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavbarCss } from './NavBar.css';
-import { Camera } from 'react-bootstrap-icons';
+import CardWidget from './CardWidget';
 
 export default class NavBar extends Component {
     render() {
@@ -11,14 +11,10 @@ export default class NavBar extends Component {
                <Container fluid className="NavBar"> 
                    <Row className="NavBar-brand">
                       <Col><h1><FaQuinscape/> WORLD FINTECH</h1></Col>
-                      <Col className="NavBar-ul"><p><NavLink className="NavBar-links">PRODUCTOS</NavLink></p></Col>
+                      <Col className="NavBar-ul"><p><NavLink className="NavBar-links"><CardWidget /> PRODUCTOS</NavLink></p></Col>
                       <Col className="NavBar-ul"><p><NavLink className="NavBar-links"><FaUserAlt/> INGRESO | REGISTRO</NavLink></p></Col>
-                   </Row>
-                  
+                   </Row>                  
                 </Container> 
-              
-             
-           
         )
       }
 }
