5cca9d6281c6b776431c39b5fa7a3370adc85b2e
pilar <pilarcayo33@gmail.com>
2021-06-07 20:31:16 -0300

    segundo commit

---
+++ src/Components/CardWidget.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+import { FaCartPlus  } from "react-icons/fa";
+import ItemListContainer from "./ItemListContainer";
+
+ export default class CardWidget extends Component {
+    render() {
+        return( 
+            <FaCartPlus/> 
+        );
+      };
+}; 
---
+++ src/Components/ItemListContainer.js
@@ -0,0 +1,7 @@
+import { react } from 'react';
+const ItemListContainer = ({greeting}) => {
+    return (  
+              <p><h3>{greeting}</h3></p>              
+           ); 
+  };
+  export default ItemListContainer;
